b27dd698d674546dd28d81528f0c1a454819eb67
Jerry <jerry.olds@mavs.uta.edu>
2020-06-23 04:41:59 -0500

    I learned that JavaScript has no native sleep function so my code was failing and JerryBot went offline. Attempt #2 to get music to work

--- commands/play.js
+++ commands/play.js
@@ -1,34 +1,37 @@
 const ytdl = require("ytdl-core");
 
-module.exports = {
+module.exports = 
+{
   name: "play",
   description: "Play a song in your channel!",
-  async execute(message) {
-    try {
+  async execute(message) 
+  {
+    try 
+    {
       const args = message.content.split(" ");
       const queue = message.client.queue;
       const serverQueue = message.client.queue.get(message.guild.id);
 
       const voiceChannel = message.member.voice.channel;
       if (!voiceChannel)
-        return message.channel.send(
-          "You need to be in a voice channel to play music!"
-        );
+        return message.channel.send("You need to be in a voice channel to play music!");
       const permissions = voiceChannel.permissionsFor(message.client.user);
-      if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-          "I need the permissions to join and speak in your voice channel!"
-        );
+      if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) 
+      {
+        return message.channel.send("I need the permissions to join and speak in your voice channel!");
       }
 
       const songInfo = await ytdl.getInfo(args[1]);
-      const song = {
+      const song = 
+      {
         title: songInfo.title,
         url: songInfo.video_url
       };
 
-      if (!serverQueue) {
-        const queueContruct = {
+      if (!serverQueue) 
+      {
+        const queueContruct = 
+        {
           textChannel: message.channel,
           voiceChannel: voiceChannel,
           connection: null,
@@ -41,25 +44,37 @@ module.exports = {
 
         queueContruct.songs.push(song);
 
-        try {
+        try 
+        {
           var connection = await voiceChannel.join();
           queueContruct.connection = connection;
           this.play(message, queueContruct.songs[0]);
-        } catch (err) {
+        } 
+        catch (err) 
+        {
           console.log(err);
           queue.delete(message.guild.id);
           return message.channel.send(err);
         }
-      } else {
+      } 
+
+      else 
+      {
         serverQueue.songs.push(song);
-        return message.channel.send(
-          `${song.title} has been added to the queue!`
-        );
+        return message.channel.send(`${song.title} has been added to the queue!`);
       }
-    } catch (error) {
+
+    } 
+    
+    catch (error) 
+    {
       console.log(error);
       message.channel.send(error.message);
     }
+
+	if (voiceChannel.members.size == 0)
+      voiceChannel.disconnect();
+
   },
 
   play(message, song) {
@@ -80,15 +95,7 @@ module.exports = {
         if(serverQueue.songs[0])
         {
           this.play(message, serverQueue.songs[0]);
-        }
-        else
-        {
-          await sleep(300000);
-          if(!serverQueue.songs[0])
-          {
-            serverQueue.connection.disconnect();
-          }
-        }
+  		}
       })
       .on("error", error => console.error(error));
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
