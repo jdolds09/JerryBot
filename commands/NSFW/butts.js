11bd343d2c48c8e8f77761019cf8bb7b30404f01
Jerry Olds <jdolds97@att.net>
2020-06-26 23:59:45 -0500

    Added async and await keywords

--- commands/NSFW/butts.js
+++ commands/NSFW/butts.js
@@ -3,30 +3,30 @@ const Search = require('random-puppy');
 module.exports = {
 	name: 'butts',
 	description: 'Post an image of a butt (Must be in NSFW channel).',
-	execute(message) {
+	async execute(message) {
         if(!message.channel.nsfw)
             message.channel.send("Must be in a NSFW channel.");
         else
         {
             try
             {
-            let subreddits = ["ass", "CuteLittleButts", "reversecowgirl",
-            "facedownassup", "butt", "butts", "cosplaybutts", "girlsinyogapants", 
-            "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
-            "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
-            "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", "Redheadass", "beautifulbutt"];
-            
-            var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
-            Search(sub)
-                .then(url => {
-                    const embed = new Discord.MessageEmbed()
-                        .setImage(url)
-                        .setColor('RANDOM')
-                        .setURL(url)
-                        .setAuthor(url);
-                    return message.channel.send({embed});
-                })
-            }
+                let subreddits = ["ass", "CuteLittleButts", "reversecowgirl",
+                "facedownassup", "butt", "butts", "cosplaybutts", "girlsinyogapants", 
+                "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
+                "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
+                "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", "Redheadass", "beautifulbutt"];
+                
+                var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
+                await Search(sub)
+                    .then(url => {
+                        const embed = new Discord.MessageEmbed()
+                            .setImage(url)
+                            .setColor('RANDOM')
+                            .setURL(url)
+                            .setAuthor(url);
+                        return message.channel.send({embed});
+                    })
+                }
 
             catch (error)
             {
