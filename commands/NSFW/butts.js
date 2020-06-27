372184371acfbd702bbaf9a19c2f96d83407aedb
Jerry Olds <jdolds97@att.net>
2020-06-27 08:42:04 -0500

    Removed async from function

--- commands/NSFW/butts.js
+++ commands/NSFW/butts.js
@@ -4,14 +4,13 @@ const Client = new Images.Client();
 module.exports = {
 	name: 'butts',
 	description: 'Post an image of a butt (Must be in NSFW channel).',
-    async execute(message) 
+    execute(message) 
     {
         try
         {
-            Client.nsfw.real.butts()
-                .then(json => {
-                    console.log(json);
-                })
+            Client.nsfw.real.butts().then(json => {
+                console.log(json);
+            })
         }
         catch(error)
         {
