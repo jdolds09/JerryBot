18a3a7f34c78949f76a07d27f72c5b673727c96a
Jerry Olds <jdolds97@att.net>
2020-06-27 05:41:42 -0500

    Made changes to random-puppy as it is depreciated

--- commands/NSFW/butts.js
+++ commands/NSFW/butts.js
@@ -1,39 +1,43 @@
-const {extname} = require('path');
-const fetch = require('node-fetch');
 const rp = require('random-puppy');
 
 module.exports = {
 	name: 'butts',
 	description: 'Post an image of a butt (Must be in NSFW channel).',
-	async execute(message) {
+    async execute(message) 
+    {
+        let subreddits = ["ass", "CuteLittleButts", "reversecowgirl",
+                "facedownassup", "butt", "butts", "cosplaybutts", "girlsinyogapants", 
+                "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
+                "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
+                "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", "Redheadass", "beautifulbutt"];
+                
+        var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
+
+
         if(!message.channel.nsfw)
             message.channel.send("Must be in a NSFW channel.");
         else
         {
             try
             {
-                let subreddits = ["ass", "CuteLittleButts", "reversecowgirl",
-                "facedownassup", "butt", "butts", "cosplaybutts", "girlsinyogapants", 
-                "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
-                "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
-                "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", "Redheadass", "beautifulbutt"];
-                
-                var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
-                
-                const url = await rp(sub);
-                const result = await fetch(url);
-                await message.channel.send({
-                    files: [{
-                        attachment: result.body,
-                        name: `image${extname(url)}`
-                    }]
-                }) 
+                var result = await rp(sub, "Reddit");
+                message.channel.send(result);
             }
 
             catch (error)
             {
                 console.log(error);
-                message.channel.send("There was an issue fetching a butt pic.");
+                try
+                {
+                    var result = await rp(sub, "Imgur");
+                    message.channel.send(result);
+                }
+
+                catch(err)
+                {
+                    console.log(error);
+                    message.channel.send(err);
+                }
             }
         }
 	},
