a5e117f9d293ea2b397836be032b9a2907f0b1c3
Jerry Olds <jdolds97@att.net>
2020-06-27 07:17:59 -0500

    changed result variable to const

--- commands/NSFW/butts.js
+++ commands/NSFW/butts.js
@@ -5,37 +5,31 @@ module.exports = {
 	description: 'Post an image of a butt (Must be in NSFW channel).',
     async execute(message) 
     {
-        try
+        let subreddits = ["ass", "CuteLittleButts", "reversecowgirl",
+                "facedownassup", "butt", "butts", "cosplaybutts", "girlsinyogapants", 
+                "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
+                "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
+                "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", 
+                "Redheadass", "beautifulbutt"];
+                
+        var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
+
+        if(!message.channel.nsfw)
+            return message.channel.send("Must be in a NSFW channel.");
+
+        else
         {
-            let subreddits = ["ass", "CuteLittleButts", "reversecowgirl",
-                    "facedownassup", "butt", "butts", "cosplaybutts", "girlsinyogapants", 
-                    "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
-                    "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
-                    "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", "Redheadass", "beautifulbutt"];
-                    
-            var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
-
-            if(!message.channel.nsfw)
-                return message.channel.send("Must be in a NSFW channel.");
-
-            else
+            try
             {
-                try
-                {
-                    var result = await rp(sub, "Reddit");
-                    message.channel.send(result);
-                }
-
-                catch (error)
-                {
-                    console.log(error);
-                    message.channel.send("Imgur pic");
-                }
+                const result = await rp(sub, "Reddit");
+                message.channel.send(result);
             }
-        }
-        catch(err)
-        {
-            console.log(err);
+
+            catch (error)
+            {
+                console.log(error);
+                message.channel.send("Imgur pic");
+            }
         }
     },
 /*
