0d30ef41f4f892c7470e3c23d03e463d2dce12e2
Jerry Olds <jdolds97@att.net>
2020-06-26 23:49:41 -0500

    Trying to correctly fetch images from reddit

--- commands/NSFW/butts.js
+++ commands/NSFW/butts.js
@@ -1,4 +1,5 @@
-const requestHandler = require("../handler");
+const Search = require('random-puppy');
+
 module.exports = {
 	name: 'butts',
 	description: 'Post an image of a butt (Must be in NSFW channel).',
@@ -12,8 +13,18 @@ module.exports = {
             "smalltitsbigass", "CelebrityButts", "booty", "NSFW_Pussy_Teen_Ass", "nsfw_college_ass",
             "beautiful_asses", "AsianAss", "cheekyasian", "ThatPerfectAss", "Top_Tier_Asses", "WhiteCheeks",
             "ASSians", "cuteAssCuterface", "AsianAsses", "CuteLittleButts", "brunetteass", "Blondeass", "Redheadass", "beautifulbutt"];
-            result = requestHandler.makeRequest("reddit", subreddits[Math.floor(Math.random() *subreddits.length)]);
-            console.log(result);
+            
+            var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
+            Search(sub)
+                .then(url => {
+                    const embed = new Discord.MessageEmbed()
+                        .setImage(url)
+                        .setColor('RANDOM')
+                        .setURL(url)
+                        .setAuthor(url);
+                })
+            
+            return message.channel.send({embed});
         }
 	},
 };
