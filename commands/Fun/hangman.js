48c758bfaf6f3a1147a137eef47af25a0de18bd7
Jerry Olds <jdolds97@att.net>
2020-06-28 18:44:27 -0500

    Removed hangman object and made it a struct

--- commands/Fun/hangman.js
+++ commands/Fun/hangman.js
@@ -1,5 +1,4 @@
-const Hangman = require("../../classes/Hangman"); // Hangman class to get variables
-const Attachment = require('discord.js'); // Needed to send Hangman images
+const Attachment = require('discord.js'); // Needed to send hangman images
 const random_word = require('random-words'); // Needed to create new word when player wins or loses
 
 // Hangman command
@@ -8,29 +7,42 @@ module.exports = {
 	description: 'Play hangman game.',
     execute(message) 
     {
-        // Get letter used after !Hangman command
+        // If hangman hasn't been created
+            if(!hangman)
+            {
+                const hangman =
+                {
+                    word: random_word(),
+                    word_length: word.length,
+                    letters: [],
+                    hits: 0,
+                    strikes: 0
+                };
+            }
+
+        // Get letter used after !hangman command
         const args = message.content.split(" ");
         const letter = args[1].charAt(0);
 
         // If letter guessed is in word
-        if(Hangman.word.includes(letter))
+        if(hangman.word.includes(letter))
         {
             // Add letters guessed correctly to array 
-            Hangman.letters.push(letter);
+            hangman.letters.push(letter);
 
-            // Output picture of current state of Hangman game
-            const attachment = new Attachment(`../../images/hangman_${Hangman.strikes}.png`);
+            // Output picture of current state of hangman game
+            const attachment = new Attachment(`../../images/hangman_${hangman.strikes}.png`);
             message.channel.send(attachment);
 
             // Output current state of game
             var i = 0;
 
-            for(i = 0; i < Hangman.word_length; i++)
+            for(i = 0; i < hangman.word_length; i++)
             {
-                if(Hangman.letters.includes(Hangman.word.charAt(i)))
+                if(hangman.letters.includes(hangman.word.charAt(i)))
                 {
                     message.channel.send(`${letter} `);
-                    Hangman.hits = Hangman.hits + 1;
+                    hangman.hits = hangman.hits + 1;
                 }
                 else
                 {
@@ -39,36 +51,36 @@ module.exports = {
             }
 
             // Check to see if player won
-            if(Hangman.hits == Hangman.word_length)
+            if(hangman.hits == hangman.word_length)
             {
                 message.channel.send("**YOU WIN!**");
-                Hangman.word = random_word();
-                Hangman.word_length = Hangman.word.length;
-                Hangman.hits = 0;
-                Hangman.strikes = 0;
-                Hangman.letters = [];
+                hangman.word = random_word();
+                hangman.word_length = hangman.word.length;
+                hangman.hits = 0;
+                hangman.strikes = 0;
+                hangman.letters = [];
             }
         }
 
         // If letter guessed is not in word
-        if(Hangman.word.includes(letter))
+        if(hangman.word.includes(letter))
         {
             // Add one to letters incorrectly guessed
-            Hangman.strikes = Hangman.strikes + 1;
+            hangman.strikes = hangman.strikes + 1;
 
-            // Output picture of current state of Hangman game
-            const attachment = new Attachment(`../../images/hangman_${Hangman.strikes}.png`);
+            // Output picture of current state of hangman game
+            const attachment = new Attachment(`../../images/hangman_${hangman.strikes}.png`);
             message.channel.send(attachment);
 
             // Output current state of game
             var i = 0;
 
-            for(i = 0; i < Hangman.word_length; i++)
+            for(i = 0; i < hangman.word_length; i++)
             {
-                if(Hangman.letters.includes(Hangman.word.charAt(i)))
+                if(hangman.letters.includes(hangman.word.charAt(i)))
                 {
                     message.channel.send(`${letter} `);
-                    Hangman.hits = Hangman.hits + 1;
+                    hangman.hits = hangman.hits + 1;
                 }
                 else
                 {
@@ -77,14 +89,14 @@ module.exports = {
             }
 
             // Check to see if player lost
-            if(Hangman.strikes == 6)
+            if(hangman.strikes == 6)
             {
                 message.channel.send("**HAHAHAHA YOU LOST DUMBASS!**");
-                Hangman.word = random_word();
-                Hangman.word_length = Hangman.word.length;
-                Hangman.hits = 0;
-                Hangman.strikes = 0;
-                Hangman.letters = [];
+                hangman.word = random_word();
+                hangman.word_length = hangman.word.length;
+                hangman.hits = 0;
+                hangman.strikes = 0;
+                hangman.letters = [];
             }
         }
     },
