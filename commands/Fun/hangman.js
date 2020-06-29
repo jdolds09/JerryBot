1608f8a1c4420292a0068b5904b24428d885493f
Jerry Olds <jdolds97@att.net>
2020-06-29 15:56:10 -0500

    Fixing problem where one hangman command was entered and game would end

--- commands/Fun/hangman.js
+++ commands/Fun/hangman.js
@@ -1,4 +1,5 @@
-const random_word = require('random-words');
+const Client = require('../../classes/Client'); // To listen for messages
+const random_word = require('random-words'); // To create a random word
 
 // Hangman command
 module.exports = {
@@ -12,86 +13,92 @@ module.exports = {
         var letters = [];
         var hits = 0;
         var strikes = 0;
+        const client = new Client();
 
-        while(!game_over && !quit)
+        message.channel.send("**Welcome to Hangman game! Type a letter or type quit to stop!**");
+
+        do
         {
-            // Get letter used after !hangman command
-            const args = message.content.split(" ");
-            if(args[1].toLowerCase() == "quit")
-                quit = true;
-            else
+            client.on('message', async message => 
             {
-                const letter = args[1].charAt(0);
-
-                // If letter guessed is in word
-                if(word.includes(letter))
-                {
-                    // Add letters guessed correctly to array 
-                    letters.push(letter);
-
-                    // Output picture of current state of hangman game
-                    message.channel.send("", {files: [`./images/hangman_${strikes}.png`]});
-
-                    // Output current state of game
-                    var i = 0;
-                    var str = "";
-                    for(i = 0; i < word.length; i++)
-                    {
-                        if(letters.includes(word.charAt(i)))
-                        {
-                            str += `${letter} `;
-                            hits += 1;
-                        }
-                        else
-                        {
-                            str += "- ";
-                        }
-                    }
-
-                    message.channel.send(str);
-
-                    // Check to see if player won
-                    if(hits == word.length)
-                    {
-                        message.channel.send("**YOU WIN!**");
-                        game_over = true;
-                    }
-                }
-
-                // If letter guessed is not in word
+                // Get letter used after !hangman command
+                const args = message.content.split(" ");
+                if(args[0].toLowerCase() == "quit")
+                    quit = true;
                 else
                 {
-                    // Add one to letters incorrectly guessed
-                    strikes += 1;
+                    const letter = args[0].charAt(0);
 
-                    // Output picture of current state of hangman game
-                    message.channel.send("", {files: [`./images/hangman_${strikes}.png`]});
-
-                    // Output current state of game
-                    var i = 0;
-                    var str = "";
-                    for(i = 0; i < word.length; i++)
+                    // If letter guessed is in word
+                    if(word.includes(letter))
                     {
-                        if(letters.includes(word.charAt(i)))
+                        // Add letters guessed correctly to array 
+                        letters.push(letter);
+
+                        // Output picture of current state of hangman game
+                        message.channel.send("", {files: [`./images/hangman_${strikes}.png`]});
+
+                        // Output current state of game
+                        var i = 0;
+                        var str = "";
+                        for(i = 0; i < word.length; i++)
                         {
-                            str += `${letter} `;
+                            if(letters.includes(word.charAt(i)))
+                            {
+                                str += `${letter} `;
+                                hits += 1;
+                            }
+                            else
+                            {
+                                str += "- ";
+                            }
                         }
-                        else
+
+                        message.channel.send(str);
+
+                        // Check to see if player won
+                        if(hits == word.length)
                         {
-                            str += '- ';
+                            message.channel.send("**YOU WIN!**");
+                            game_over = true;
                         }
                     }
 
-                    message.channel.send(str);
-
-                    // Check to see if player lost
-                    if(strikes == 6)
+                    // If letter guessed is not in word
+                    else
                     {
-                        message.channel.send("**HAHAHAHA YOU LOST DUMBASS!**");
-                        game_over = true;
+                        // Add one to letters incorrectly guessed
+                        strikes += 1;
+
+                        // Output picture of current state of hangman game
+                        message.channel.send("", {files: [`./images/hangman_${strikes}.png`]});
+
+                        // Output current state of game
+                        var i = 0;
+                        var str = "";
+                        for(i = 0; i < word.length; i++)
+                        {
+                            if(letters.includes(word.charAt(i)))
+                            {
+                                str += `${letter} `;
+                            }
+                            else
+                            {
+                                str += '- ';
+                            }
+                        }
+
+                        message.channel.send(str);
+
+                        // Check to see if player lost
+                        if(strikes == 6)
+                        {
+                            message.channel.send("**HAHAHAHA YOU LOST DUMBASS!**");
+                            game_over = true;
+                        }
                     }
                 }
-            }
-        }
+            });
+        }while(!game_over && !quit);
     },
 };
