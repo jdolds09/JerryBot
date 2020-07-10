8f8e445bd0752dfc0827370c97a0d7b2e54acf9b
Jerry Olds <jdolds97@att.net>
2020-07-09 20:18:38 -0500

    Removed playlist functionality because youtube allows very little usage of their api needed to play playlists

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,28 +1,19 @@
-const ytdl = require("ytdl-core"); // For youtube functions
-const YouTube = require("simple-youtube-api"); // For youtube playlist
+const ytdl = require("ytdl-core");
 
-const youtube = new YouTube("AIzaSyCkMLHynwJgJYQoGaIResXZxKUbC2euFfw");
-
-// Play command
 module.exports = {
   name: "play",
   description: "Play a song in your channel!",
   async execute(message) {
     try {
-      // Get youtube link after !play command
       const args = message.content.split(" ");
-
-      // Create song queue
       const queue = message.client.queue;
       const serverQueue = message.client.queue.get(message.guild.id);
 
-      // Get voice channel and check if user is currently in voice channel
       const voiceChannel = message.member.voice.channel;
       if (!voiceChannel)
         return message.channel.send(
-          "You need to be in a voice channel to play music dumbass."
+          "You need to be in a voice channel to play music!"
         );
-      // Check to see if JerryBot has permissions to join and play music
       const permissions = voiceChannel.permissionsFor(message.client.user);
       if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
         return message.channel.send(
@@ -30,7 +21,12 @@ module.exports = {
         );
       }
 
-      // If queue hasnt been created yet
+      const songInfo = await ytdl.getInfo(args[1]);
+      const song = {
+        title: songInfo.title,
+        url: songInfo.video_url
+      };
+
       if (!serverQueue) {
         const queueContruct = {
           textChannel: message.channel,
@@ -43,129 +39,40 @@ module.exports = {
 
         queue.set(message.guild.id, queueContruct);
 
-        // If bot is not currently playing a song and user wants to play a playlist
-        if(args[1].match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/))
-        {
-          // Get playlist info
-          const playlist = await youtube.getPlaylist(args[1]);
-          const videos = await playlist.getVideos();
-          
-          // Add all videos in playlist to queue 
-          for (const video of Object.values(videos))
-          {
-            const video2 = await youtube.getVideoByID(video.id);
-            
-            const song = {
-            title: video2.title,
-            url: `https://www.youtube.com/watch?v=${video2.id}`
-            };
+        queueContruct.songs.push(song);
 
-            queueContruct.songs.push(song);
-          }
-
-          // Start playing first song in playlist
-          try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            this.play(message, queueContruct.songs[0]);
-          } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
-          }
+        try {
+          var connection = await voiceChannel.join();
+          queueContruct.connection = connection;
+          this.play(message, queueContruct.songs[0]);
+        } catch (err) {
+          console.log(err);
+          queue.delete(message.guild.id);
+          return message.channel.send(err);
         }
-
-        // Bot is not currently playing a song and user wants to play a single song
-        else
-        {
-          // Get song info
-          const songInfo = await ytdl.getInfo(args[1])
-          const song = 
-          {
-            title: songInfo.title,
-            url: songInfo.video_url
-          };
-
-          // Push the song onto the queue
-          queueContruct.songs.push(song);
-
-          // Play the song given in the command
-          try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            this.play(message, queueContruct.songs[0]);
-          } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
-          }
-        }
+      } else {
+        serverQueue.songs.push(song);
+        return message.channel.send(
+          `${song.title} has been added to the queue!`
+        );
       }
-
-      // Song is currently playing and user wants to queue up a playlist
-      else 
-      {
-        if(args[1].match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/))
-        {
-          // Get playlist info
-          const playlist = await youtube.getPlaylist(args[1]);
-          const videos = await playlist.getVideos();
-
-          // Add videos in playlist to queue
-          for (const video of Object.values(videos))
-          {
-            const video2 = await youtube.getVideoByID(video.id);
-            
-            const song = {
-            title: video2.title,
-            url: `https://www.youtube.com/watch?v=${video2.id}`
-            };
-
-            queueContruct.songs.push(song);
-          }
-          return message.channel.send(`**${playlist.title}** has been added to the queue.`);
-        }
-
-        // Song is currently playing and user wants to add another song to queue
-        else
-        {
-          // Get song info
-          const songInfo = await ytdl.getInfo(args[1])
-          const song = 
-          {
-            title: songInfo.title,
-            url: songInfo.video_url
-          };
-
-          // Add song to queue
-          serverQueue.songs.push(song);
-          return message.channel.send(
-            `**${song.title}** has been added to the queue!`
-          );
-        }
-      }
-      
     } catch (error) {
       console.log(error);
       message.channel.send(error.message);
     }
   },
 
-// Play song function
   play(message, song) {
-    // Get queue
     const queue = message.client.queue;
     const guild = message.guild;
     const serverQueue = queue.get(message.guild.id);
 
-    // Invalid song link
     if (!song) {
       serverQueue.voiceChannel.leave();
       queue.delete(guild.id);
       return;
     }
 
-    // Play song
     const dispatcher = serverQueue.connection
       .play(ytdl(song.url))
       .on("finish", () => {
