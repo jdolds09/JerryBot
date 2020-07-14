685b60bb4ced282f59321def046c42ff89cf5d14
Jerry Olds <jdolds97@att.net>
2020-07-14 17:29:37 -0500

    Making JerryBot music volume perfect

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -93,7 +93,7 @@ module.exports = {
         this.play(message, serverQueue.songs[0]);
       })
       .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 11);
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 12);
     serverQueue.textChannel.send(`Start playing: **${song.title}**`);
   }
 };
