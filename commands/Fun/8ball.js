6a434d403138f3f2475dfa8b1ec5559e59054c50
Jerry Olds <jdolds97@att.net>
2020-07-15 02:05:18 -0500

    Added two more responses to 8ball command

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -6,11 +6,15 @@ module.exports = {
     {
         try
         {
-            const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother.", "Negative.", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit.", "Best Fucking Believe.", "Nope."];
+            const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother.", "Negative.", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit.", "Best Fucking Believe.", "Nope.", "Yup,", "Gif"];
 
             const result = Math.floor((Math.random() * responses.length));
 
-            return message.channel.send(`**${responses[result]}**`)
+            if(responses[result] == "Gif")
+                message.channel.send("", {files: ['./images/no.gif']});
+
+            else
+                return message.channel.send(`**${responses[result]}**`)
         }
         catch(error)
         {
