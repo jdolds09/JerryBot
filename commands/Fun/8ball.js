734c24c77b0edaefce02dbb442b44c667cd63014
Jerry Olds <jdolds97@att.net>
2020-07-15 01:25:11 -0500

    Fixed issue where random number generated exceeded size of responses array

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother,", "Negative,", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit."];
 
-            const result = Math.floor((Math.random() * 100) + 1);
+            const result = Math.floor((Math.random() * responses.length) + 1);
 
             return message.channel.send(`**${responses[result]}**`)
         }
