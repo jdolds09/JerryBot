d811f49e1524d0a7de9f025d214d90ba278a5c47
Jerry Olds <jdolds97@att.net>
2020-07-15 02:09:57 -0500

    Removed comma in yup 8 ball response

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -6,7 +6,7 @@ module.exports = {
     {
         try
         {
-            const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother.", "Negative.", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit.", "Best Fucking Believe.", "Nope.", "Yup,", "Gif"];
+            const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother.", "Negative.", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit.", "Best Fucking Believe.", "Nope.", "Yup.", "Gif"];
 
             const result = Math.floor((Math.random() * responses.length));
 
