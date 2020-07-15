10fa1de273f3b9b12ade042c0a8d1f96eda6e974
Jerry Olds <jdolds97@att.net>
2020-07-15 01:37:08 -0500

    Trying to fix undefined error in 8ball command and removed outputted commas

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -6,9 +6,9 @@ module.exports = {
     {
         try
         {
-            const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother,", "Negative,", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit."];
+            const responses = ["Fuck No.", "No Fucking Way.", "Abso-Fucking-Lutely.", "100 Fucking Percent dude.", "How the Fuck am I supposed to know?", "You're Goddamn Right.", "Haaaaaaaaail Nah.", "You Bet Your Ass.", "Who the Fuck Knows.", "Without a Fucking Doubt.", "Not Even Fucking Close.", "I Don't Give a Shit.", "Alex likes wieners. That is the Answer.", "Hell Yea Brother.", "Negative.", "Does a Bear Shit in the Woods?", "No way in Hell.", "You Bet your Ass.", "That's Horseshit."];
 
-            const result = Math.floor((Math.random() * responses.length) + 1);
+            const result = Math.floor((Math.random() * responses.length));
 
             return message.channel.send(`**${responses[result]}**`)
         }
