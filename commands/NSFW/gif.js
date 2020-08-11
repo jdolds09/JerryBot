6ab02d6d2aaf6b9dc16334708b13618686df7bb5
Jerry Olds <jdolds97@att.net>
2020-08-11 03:36:10 -0500

    Added test command gif

---
+++ commands/NSFW/gif.js
@@ -0,0 +1,25 @@
+const Images = require("dabi-images");
+const Client = new Images.Client();
+
+// Butts command
+module.exports = {
+	name: 'gif',
+	description: 'Post an image of a butt (Must be in NSFW channel).',
+    execute(message) 
+    {
+        // Check to see if command was sent in NSFW channel
+        if(!message.channel.nsfw)
+            message.channel.send("Must be in a NSFW channel.");
+        else
+        {
+            // Get butt image
+            Client.nsfw.real.gif().then(json => {
+                // Send image
+                return message.channel.send(json.url);
+                }).catch(error => {
+                    message.channel.send("Unable to fetch image. Please try again.");
+                    console.log(error);
+                });
+        }
+    },
+};
