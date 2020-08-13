3a632d983492ab669d421c9ecbe158c82c101be7
Jerry Olds <jdolds97@att.net>
2020-08-13 16:13:29 -0500

    Added functionality to skip multiple songs

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -3,13 +3,27 @@ module.exports = {
 	name: 'skip',
 	description: 'Skip a song!',
 	execute(message) {
+		// Get number after !skip command if it exists
+		const args = message.content.split(" ");
 		// Get queue
 		const serverQueue = message.client.queue.get(message.guild.id);
 		// Must be in voice channel to skip song
 		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
 		// Unable to skip song
 		if (!serverQueue) return message.channel.send('There is no song that I could skip!');
-		// Skip song
-		serverQueue.connection.dispatcher.end();
+
+		// If number after !roll exists
+        if(args.length > 1)
+        {
+			var i = 0;
+			for(i = 0; i < args[1]; i++)
+				serverQueue.connection.dispatcher.end();
+		}
+		
+		else
+		{
+			// Skip song
+			serverQueue.connection.dispatcher.end();
+		}
 	},
 };
