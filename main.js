2783c35e35638a432d3907d743777c478d8aa175
Jerry Olds <jdolds97@att.net>
2020-10-10 20:55:22 -0500

    Got rid of conversation bot code for now

--- main.js
+++ main.js
@@ -11,11 +11,6 @@ var servers = {}
 // Declare command variale
 const client = new Client();
 client.commands = new Discord.Collection();
-const convo = new Discord.Client({
-    disableEveryone: true,
-    disabledEvents: ['CHANNEL_PINS_UPDATE', 'GUILD_BAN_ADD', 'GUILD_BAN_REMOVE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
-});
-convo.util = require('./classes/util');
 
 // Get all commands
 const music_command_files = fs.readdirSync('./commands/Music').filter(file => file.endsWith('.js'));
@@ -57,12 +52,6 @@ client.once('ready', () => {
 client.on('message', async message => {
 
     var msg = message.content.toLowerCase();
-
-    // Conversation bot
-    if(msg.content.startsWith(`<@${msg.Client.user.id}>`) || msg.content.startsWith(`<@!${msg.Client.user.id}>`))
-    {
-        convo.util.handleTalk(msg);
-    }
 
     // Twitch emote reactions
     if(msg.includes("monka"))
