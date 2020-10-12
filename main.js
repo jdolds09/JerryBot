b0b1aef63eb913a4cbcd73db018685610aad17c6
Jerry Olds <jdolds97@att.net>
2020-10-12 18:40:26 -0500

    Trying to fix reply functionality

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ client.on('message', async message => {
 
     var msg = message.content.toLowerCase();
     
-    if(message.content.startsWith('balls') || message.content.startsWith('<@723893316592074782>') || message.content.startsWith('<@!723893316592074782>'))
+    if(message.content.startsWith('balls') || message.content.startsWith(client2.user))
         message.reply("fuck");
 
     // Twitch emote reactions
