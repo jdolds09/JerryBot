5ad093c14a0d6b72c86d2c2f75754f23669b989c
Jerry Olds <jdolds97@att.net>
2020-11-04 20:53:26 -0600

    Added poll functionality

---
+++ classes/poll.js
@@ -0,0 +1,204 @@
+const Discord = require("discord.js");
+const hash = require("string-hash");
+
+const numEmojis = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣", "🔟"];
+const handEmojis = ["👍", "👎"];
+
+class Poll {
+	constructor(msg, question, answers, time, type) {
+		if (msg) { // if the constructor have parameters
+			this.guildId = msg.guild.id;
+			this.channelId = msg.channel.id;
+			this.msgId = null;
+			this.question = question;
+			this.answers = answers;
+			this.createdOn = Date.now();
+			this.isTimed = (time != 0);
+			this.hasFinished = false;
+			this.finishTime = new Date(this.createdOn + time).getTime();
+			this.type = type;
+			this.emojis = this.getEmojis(type);
+			this.results = [];
+			this.id = this.generateId();
+		}
+	}
+
+	static copyConstructor (other) {
+		let p = new Poll();
+
+		p.guildId = other.guildId;
+		p.channelId = other.channelId;
+		p.msgId = other.msgId;
+		p.question = other.question;
+		p.answers = other.answers;
+		p.createdOn = other.createdOn;
+		p.isTimed = other.isTimed;
+		p.finishTime = other.finishTime;
+		p.hasFinished = other.hasFInished;
+		p.type = other.type;
+		p.emojis = other.emojis;
+		p.results = other.results;
+		p.id = other.id;
+
+		return p;
+	}
+
+	async start(msg) {
+		const message = await msg.channel.send({ embed: this.generateEmbed() })
+		this.msgId = message.id;
+		for (let i = 0; i < this.answers.length && i < 10; ++i) {
+			try {
+				await message.react(this.emojis[i]);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		return message.id;
+	}
+
+	async finish(client) {
+		const now = new Date();
+		const message = await this.getPollMessage(client);
+		if (!message) {
+			console.error("Cant find poll message");
+			return;
+		}
+		if (message.embeds.length < 1) {
+			console.error("The poll message ha no embeds.");
+			return;
+		}
+		
+		this.hasFinished = true;
+		
+		const embed = new Discord.RichEmbed(message.embeds[0]);
+		embed.setColor("FF0800")
+			.setAuthor(`${this.question} [FINISHED]`)
+			.setFooter(`Poll ${this.id} finished ${now.toUTCString()}`);
+
+		try {
+			await message.edit({ embed: embed });
+			await this.getVotes(message);
+			await this.showResults(message.channel);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async getVotes(message) {
+		if (this.hasFinished) {
+			const reactionCollection = message.reactions;
+			for (let i = 0; i < this.answers.length; i++) {
+				this.results[i] = reactionCollection.get(this.emojis[i]).count - 1;
+			}
+		} else {
+			throw new Error("Poll not ended");
+		}
+	}
+
+	async showResults(channel) {
+		if (!this.hasFinished) {
+			throw new Error("The poll is not finished");
+		}
+		if (this.results.length < 2) {
+			throw new Error("There are no results");
+		}
+
+		return await channel.send({ embed: this.generateResultsEmbed() });
+	}
+
+	generateEmbed() {
+		let str = new String();
+
+		if (this.type !== "yn") {
+			for (let i = 0; i < this.answers.length && i < 10; i++) {
+				str += `${this.emojis[i]}. ${this.answers[i]}\n`;
+			}
+		}
+
+		let footer = `React with the emojis below | ID: ${this.id}`;
+		if (this.isTimed) footer += ` | This poll ends in ${new Date(this.finishTime).toUTCString()}`;
+
+		let embed = new Discord.RichEmbed()
+			.setColor("#50C878")
+			.setAuthor("📊" + this.question)
+			.setDescription(str)
+			.setFooter(footer);
+
+		return embed;
+	}
+
+	generateResultsEmbed() {
+		let description = new String();
+		let totalVotes = 0;
+
+		this.results.forEach((answer) => totalVotes += answer);
+		if (totalVotes == 0) totalVotes = 1;
+
+		let finalResults = [];
+
+		for (let i = 0; i < this.results.length; i++) {
+			let percentage = (this.results[i] / totalVotes * 100);
+			let result = {
+				emoji: this.emojis[i],
+				answer: this.answers[i],
+				votes: this.results[i],
+				percentage: percentage.toFixed(2)
+			}
+
+			finalResults.push(result);
+		}
+
+		if (this.type !== "yn") { // only sort if its not a yn poll
+			finalResults.sort((a, b) => { return b.votes - a.votes });
+		}
+
+		finalResults.forEach((r) => {
+			description += `${r.emoji} ${r.answer} :: ** ${r.votes} ** :: ${r.percentage}% \n`;
+		});
+
+		let footer = `Results from poll ${this.id} finished on ${new Date(this.finishTime).toUTCString()}`;
+		let resultsEmbed = new Discord.RichEmbed()
+			.setAuthor("Results of: " + this.question)
+			.setDescription(description)
+			.setFooter(footer)
+			.setColor("#0080FF");
+
+		return resultsEmbed;
+	}
+
+	generateId() {
+		let id = new String("");
+		if (this.id) {
+			id += this.id + Date.now();
+		} else {
+			const d = new Date(this.createdOn);
+			id += d.getUTCFullYear();
+			id += d.getUTCDate();
+			id += d.getUTCHours();
+			id += d.getUTCMinutes();
+			id += d.getUTCMilliseconds();
+			id += this.question;
+		}
+		this.id = hash(id);
+		return this.id;
+	}
+
+	getEmojis(type) {
+		switch (type) {
+			case "yn":
+				return handEmojis;
+			case "default":
+				return numEmojis;
+			default:
+				throw new Error("The poll type is not known");
+		}
+	}
+
+	async getPollMessage(client) {
+		try {
+			return await client.guilds.get(this.guildId).channels.get(this.channelId).fetchMessage(this.msgId);
+		} catch (err) {
+			return;
+		}
+	}
+}
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fs = require('fs') // For file parsing
 const Discord = require('discord.js'); // For Discord functions
 const Client = require('./classes/Client'); // To save commands
+const Poll = require("./classes/poll.js");
+const Datastore = require('nedb');
 
 // Create dialogflow client
 const dialogflow = require('@danclay/discord-dialogflow')
@@ -21,6 +23,159 @@ var servers = {}
 // Declare command variale
 const client = new Client();
 client.commands = new Discord.Collection();
+
+const helpEmbed = new Discord.RichEmbed()
+	        .setAuthor("VotaBot's Commands")
+            .addField("Create Y/N poll", `\`${config.prefix} "Question"\``)
+            .addField("Create complex poll [2-10 answers]", `\`${config.prefix} "Question" "Option 1" "Option 2" ["Option 3" ...]\` (quotes are necessary)`)
+            .addField("Timed polls that close automatically", `\`${config.prefix} time=X{s|m|h|d} ...\`, where "X" is the time to finish the
+                poll followed by it's unit.`)
+            .addField("See results of a poll and close the voting", `\`${config.prefix} end ID\`, where ID is the poll id wich
+                appears at the end of the poll`)
+            .addField("See examples", `\`${config.prefix} examples\``)
+            .addBlankField()
+            .addField("About", "The bot has been created by Zheoni. Find the source code [here](http://github.com/Zheoni/VotaBot). Feel free to report bugs.")
+            .setColor("#DDA0DD");
+    
+        const examplesEmbed = new Discord.RichEmbed()
+            .setAuthor("Examples of VotaBot's commands")
+            .addField("Y/N Poll", `\`${config.prefix} "Do you like this?"\``)
+            .addField("Complex poll", `\`${config.prefix} "What do you wanna play?" "Overwatch" "CS:GO" "Quake" "WoW"\``)
+            .addField("Timed poll", `\`${config.prefix} time=6h "Chat tonight?"\``)
+            .addField("See the results of a poll", `\`${config.prefix} end 61342378\``)
+            .setColor("#DDA0DD");
+    
+    
+        let database = new Datastore('database.db');
+        database.loadDatabase();
+        database.persistence.setAutocompactionInterval(3600000);
+
+        async function finishTimedPolls() {
+            const now = Date.now()
+            database.find({ isTimed: true, finishTime: { $lte: now } }, (err, dbps) => {
+                if (err) console.error(err);
+        
+                dbps.forEach((dbp) => {
+                    const p = Poll.copyConstructor(dbp);
+        
+                    if (p instanceof Poll && p.isTimed && p.finishTime <= now) {
+                        p.finish(client);
+                        database.remove({ id: p.id });
+                    }
+                });
+            });
+        }
+
+        async function poll(msg, args) {
+            const timeToVote = await parseTime(msg, args);
+        
+            const question = args.shift();
+            let answers = [];
+            let type;
+        
+            switch (args.length) {
+                case 0:
+                    answers = ["", ""];
+                    type = "yn";
+                    break;
+                case 1:
+                    msg.reply("You cannot create a poll with only one answer");
+                    return;
+                default:
+                    answers = args;
+                    type = "default";
+                    break;
+            }
+        
+            const p = await new Poll(msg, question, answers, timeToVote, type);
+        
+            await p.start(msg);
+        
+            if (p.hasFinished == false) {
+                database.insert(p);
+                // maybe we can get a duplicated id...
+            }
+        }
+
+        async function end(msg, args) {
+            const inputid = Number(args[1]);
+        
+            database.findOne({ id: inputid }, (err, dbp) => {
+                if (err) { console.errror(err); }
+                if (dbp) {
+                    const p = Poll.copyConstructor(dbp);
+                    if (!p.hasFinished && p.guildId === msg.guild.id) {
+                        p.finish(client)
+                        database.remove({ id: p.id });
+                    }
+                } else {
+                    msg.reply("Cannot find the poll.");
+                }
+            });
+        }
+
+        function parseTime(msg, args) {
+            let time = 0;
+        
+            //parse the time limit if it exists
+            if (args[0].startsWith("time=")) {
+                const timeRegex = /\d+/;
+                const unitRegex = /s|m|h|d/i;
+                let timeString = args.shift();
+                let unit = "s";
+        
+                let match;
+        
+                // check if the time is correct
+                match = timeString.match(timeRegex);
+                if (match != null) {
+                    time = parseInt(match.shift());
+                } else {
+                    msg.reply("Wrong time syntax!");
+                    return;
+                }
+        
+                // check the units of the time
+                match = timeString.split("=").pop().match(unitRegex);
+                if (match != null) unit = match.shift();
+        
+                switch (unit) {
+                    case "s": time *= 1000;
+                        break;
+                    case "m": time *= 60000;
+                        break;
+                    case "h": time *= 3600000;
+                        break;
+                    case "d": time *= 86400000;
+                        break;
+                    default: time *= 60000;
+                }
+            }
+        
+            if (time > 604800000) return 604800000; // no more than a week.
+            else return time;
+        }
+
+        function parseToArgs(msg) {
+            let args = msg.content.slice(config.prefix.length)
+                .trim()
+                .split("\"")
+                .filter((phrase) => phrase.trim() !== "");
+            for (let i = 0; i < args.length; i++)
+                args[i] = args[i].trim();
+            if (args[0].startsWith("end")) {
+                let aux = args[0].split(" ");
+                args[0] = aux[0];
+                args.push(aux[1]);
+            }
+            return args;
+        }
+
+        function cleanDatabase() {
+            console.log("Cleaning the database...");
+            const aWeekAgo = Date.now() - 604800000;
+            database.remove({ createdOn: { $lt: aWeekAgo } }, { multi: true }, (err, n) => console.log(n + " entries removed."));
+        }
 
 // Get all commands
 const music_command_files = fs.readdirSync('./commands/Music').filter(file => file.endsWith('.js'));
@@ -72,6 +227,36 @@ client.on('message', async message => {
         dialogflow.getIntent(message, (r) => {
             message.reply(r);
         });
+    }
+
+    if(msg.includes("!poll"))
+    {
+        let args = parseToArgs(msg);
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "help":
+                    dmChannel = await msg.author.createDM();
+                    await dmChannel.send({ embed: helpEmbed });
+                    dmChannel.send(helpMessage);
+                    break;
+                case "examples":
+                    dmChannel = await msg.author.createDM();
+                    dmChannel.send({ embed: examplesEmbed });
+                    break;
+                case "end":
+                    if (!isDM) {
+                        end(msg, args);
+                    }
+                    break;
+                default:
+                    if (!isDM) {
+                        poll(msg, args);
+                    }
+                    break;
+            }
+        } else {
+            msg.reply("Sorry, give me more at least a question");
+        }
     }
 
     // Twitch emote reactions
