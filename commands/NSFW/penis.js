f36cd0509ca7dcfa60755397f1e42e4729e48106
Jerry Olds <jdolds97@att.net>
2021-03-22 23:57:37 -0500

    added a new command.....

---
+++ commands/NSFW/penis.js
@@ -0,0 +1,17 @@
+// Roll command
+module.exports = {
+	name: 'penis',
+	description: 'See how big your pee pee is',
+    execute(message) 
+    {
+        const size = Math.floor(Math.random() * 11);
+        var i;
+        var shaft;
+
+        for(i = 0; i < size; i++)
+            shaft = shaft.concat("=");
+
+        message.channel.send(`${message.author.username}'s penis size:`);
+        return message.channel.send(`8${shaft}D`);
+    },
+};
