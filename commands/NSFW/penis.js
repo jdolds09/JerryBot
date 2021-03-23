8fcdb5f2d860b4d42ad4e6ef1b67289a8b1cd780
Jerry Olds <jdolds97@att.net>
2021-03-23 00:07:03 -0500

    Added special Aaron message

--- commands/NSFW/penis.js
+++ commands/NSFW/penis.js
@@ -11,6 +11,9 @@ module.exports = {
         for(i = 0; i < size; i++)
             shaft = shaft + "=";
 
+        if(message.author.username == "DrunkenMaster89")
+            shaft = "";
+
         message.channel.send(`${message.author.username}'s penis size:`);
         return message.channel.send(`8${shaft}D`);
     },
