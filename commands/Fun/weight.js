3850e971351026e1c69efdd06268a3bfc597795a
Jerry Olds <jdolds97@att.net>
2021-03-30 11:29:54 -0500

    Testing weight command

--- commands/Fun/weight.js
+++ commands/Fun/weight.js
@@ -12,11 +12,13 @@ module.exports = {
         if(!(args.length > 1))
             return message.channel.send("You must supply additional argument(s) along with the weight command.");
 
-        if(typeof args[1] == 'number')
+        if(typeof args[1] === 'number')
         {
+            message.channel.send("fuck");
             fs.writeFile(`/weight/${message.author.username}.txt`, args[1], function (err) {
                 if (err) return console.log(err);
             });
+            return message.channel.send("added file");
         }
     },
 };
