11f8e76611c095f1c33ae2f3f97a57a5b9ebfb05
Jerry Olds <jdolds97@att.net>
2021-03-31 22:56:31 -0500

    Testing weight command

--- commands/Fun/weight.js
+++ commands/Fun/weight.js
@@ -38,51 +38,6 @@ module.exports = {
                 return message.channel.send("This user is not in the database. Jerry needs to add you.");
         }
 
-        else if(Number(args[1]) != NaN)
-        {
-            if(fs.existsSync(`/app/commands/Fun/weight/${message.author.username}.txt`))
-            {
-                fs.readFile(`/app/commands/Fun/weight/${message.author.username}.txt`, 'utf8', function (err, data) {
-                    if(err) return console.log(err);
-                    if (data.length == 0)
-                        empty_file = true;
-                    else
-                    {
-                        fs.readFile(`/app/commands/Fun/weight/${message.author.username}_goal.txt`, 'utf8', function (err, goal_data) {
-                            if(err) return console.log(err);
-                            if(data.length == 0)
-                            {
-                                message.channel.send("You must enter a target weight before entering another weight.");
-                                return message.channel.send("You can enter a target weight by using the !weight target [desired weight] command.");
-                            }
-                        });
-                    }
-
-                    if(data.includes(months[month]))
-                    {
-                        var length = months[month].length + 2;
-                        if(data.substring(data.indexOf(months[month]) + length, data.indexOf(months[month]) + length + 4) == year)
-                        {
-                            message.channel.send("You have already entered a weight for this month.");
-                            return message.channel.send("It's too much damn work to go back and replace that value. Fuck you.");
-                        }
-                    }
-                    
-                    else
-                    {
-                        fs.appendFile(`/app/commands/Fun/weight/${message.author.username}.txt`, `${args[1]}\r\n${months[month]}\r\n${year}\r\n`, function (err) {
-                            if (err) return console.log(err);
-                            if(empty_file)
-                                return message.channel.send("Please enter the weight you wish to be at by using the !weight target [desired weight] command");
-                        });
-                    }
-                });
-            }
-
-            else
-                return message.channel.send("This user is not in the database. Jerry needs to add you.");
-        }
-
         else if(args[1].toLowerCase() == "target")
         {
             if(args.length < 3)
@@ -134,8 +89,53 @@ module.exports = {
             else
                 return message.channel.send("This user is not in the database. Jerry needs to add you.");
         }
+
+        else if(Number(args[1]) != NaN)
+        {
+            if(fs.existsSync(`/app/commands/Fun/weight/${message.author.username}.txt`))
+            {
+                fs.readFile(`/app/commands/Fun/weight/${message.author.username}.txt`, 'utf8', function (err, data) {
+                    if(err) return console.log(err);
+                    if (data.length == 0)
+                        empty_file = true;
+                    else
+                    {
+                        fs.readFile(`/app/commands/Fun/weight/${message.author.username}_goal.txt`, 'utf8', function (err, goal_data) {
+                            if(err) return console.log(err);
+                            if(data.length == 0)
+                            {
+                                message.channel.send("You must enter a target weight before entering another weight.");
+                                return message.channel.send("You can enter a target weight by using the !weight target [desired weight] command.");
+                            }
+                        });
+                    }
+
+                    if(data.includes(months[month]))
+                    {
+                        var length = months[month].length + 2;
+                        if(data.substring(data.indexOf(months[month]) + length, data.indexOf(months[month]) + length + 4) == year)
+                        {
+                            message.channel.send("You have already entered a weight for this month.");
+                            return message.channel.send("It's too much damn work to go back and replace that value. Fuck you.");
+                        }
+                    }
+                    
+                    else
+                    {
+                        fs.appendFile(`/app/commands/Fun/weight/${message.author.username}.txt`, `${args[1]}\r\n${months[month]}\r\n${year}\r\n`, function (err) {
+                            if (err) return console.log(err);
+                            if(empty_file)
+                                return message.channel.send("Please enter the weight you wish to be at by using the !weight target [desired weight] command");
+                        });
+                    }
+                });
+            }
+
+            else
+                return message.channel.send("This user is not in the database. Jerry needs to add you.");
+        }
     
-    else
-        return message.channel.send("Incorrect arguments supplied with the weight command");
+        else
+            return message.channel.send("Incorrect arguments supplied with the weight command");
     },
 };
