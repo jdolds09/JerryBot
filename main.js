27b94b372cf03f7b82f3ef1804bbfe160b24e8d0
Jerry Olds <jdolds97@att.net>
2021-09-11 18:27:52 -0500

    Added uhm

--- main.js
+++ main.js
@@ -449,6 +449,9 @@ client.on('message', async message => {
         else if(msg.includes("copium"))
             message.channel.send("", {files: ['./images/copium.jpeg']});
 
+        else if(msg.includes("uhm"))
+            message.channel.send("", {files: ['./images/uhm.png']});
+
         else if(msg.includes("gasm") && !msg.includes("gachi"))
             message.channel.send("", {files: ['./images/gasm.png']});
             
