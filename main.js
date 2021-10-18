92a54f2c533f54c15e2c8031cf35cbd4d1b81568
Jerry Olds <jdolds97@att.net>
2021-10-17 19:55:20 -0500

    Post every 2 hours

--- main.js
+++ main.js
@@ -732,7 +732,7 @@ client.on('message', async message => {
         {        
             const CronJob = require('node-cron');
 
-            var job = CronJob.schedule('0 0 */2 * * *', function() {
+            var job = CronJob.schedule('* * */2 * * *', function() {
                 for(i = 0; i < 5; i++)
                 {
                     // Fetch butts image
